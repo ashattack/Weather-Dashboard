beaf7acd3f60d2983769cb301563e81b62d66fb4
Ashlyn Hicks <ash.giselle96@gmail.com>
2021-01-07 23:28:00 -0500

    trying to add weather

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Weather-Dashboard
 Homework for UCF Bootcamp
 https://ashattack.github.io/Weather-Dashboard/
+
+
+## Directions 
+
+![gif](06-server-side-apis-homework-demo.png)
+
+## About
+
+Calls two openweathermap APIS
+- https://openweathermap.org/current
+- https://openweathermap.org/api/one-call-api
--- logic.js
+++ logic.js
@@ -5,7 +5,7 @@ const LOCAL_STORAGE_KEY = "weather-dashboard-history"
 
 //calling the API key
 function searchWeather(searchValue, callback) {
-    fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${searchValue}&APPID=${API_KEY}`)
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${searchValue}&APPID=${API_KEY}`)
         //a promise
         .then(res => {
             return res.json();
@@ -17,7 +17,6 @@ function searchWeather(searchValue, callback) {
         }).catch(err => {
             console.log('catch error')
             console.log(err)
-            callback(err)
         })
 }
 
@@ -26,12 +25,60 @@ function saveHistory(searchValue) {
     localStorage.setItem(LOCAL_STORAGE_KEY, localhistory + `\n${searchValue}`)
 }
 
+function getOneCallApi(coords, callback) {
+    fetch(`http://api.openweathermap.org/data/2.5/onecall?lat=${coords.lat}&lon=${coords.lon}&APPID=${API_KEY}`)
+        //a promise
+        .then(res => {
+            return res.json();
+        }).then(function (res) {
+            console.log(res);
+            //is a function that is postFiveDayForecast
+            callback(res)
+            //if there are errors
+        }).catch(err => {
+            console.log('catch error')
+            console.log(err)
+        })
+}
+
+function writeCurrentConditionsToDom(res) {
+
+    var today = $("<h2>").text(searchValue + ": " + date);
+    var icon = $("<img>");
+    weatherIcon.attr("src", "http://openweathermap.org/img/wn/" + response.weather[0].icon + ".png");
+    todaysWeather.append(weatherIcon);
+    console.log(weatherIcon);
+    let temperate = $("div").text("Temperature: " + response.main.temp);
+    let humidity = $("div").text("Humidity: " + response.main.humidity);
+    let windSpeed
+
+}
+
+function addUvi(uvi) {
+    $("#uti").text(`UVI: ${uvi}`)
+}
+
+function writeFiveDaysToDom(res) {
+    // addUvi(res.current.uvi)
+    let forcast = res.daily.slice(1, 5)
+    $("#nextfivedays").html(forcast.map(x => `
+    <div class="card">
+    <div class="card-body">
+        <h4>${x.dt}</h4>
+        <h6>Temp ${x.max}</h6>
+        <h6>Temp ${x.humidity}</h6>
+    </div>
+  </div>`))
+}
+
 function postFiveDayForecast(result) {
     if (result.cod == "200") {
         // yaaaaa a city
-        saveHistory(result.city.name)
+        saveHistory(result.name)
         loadHistory()
+        writeCurrentConditionsToDom(result)
         // make html dance for the fellas
+        getOneCallApi(result.coord, writeFiveDaysToDom)
     } else if (result.cod == "404") {
         // not a citty
         // log err msg saying city not found
@@ -42,10 +89,13 @@ function postFiveDayForecast(result) {
 
 function loadHistory() {
     const localhistory = localStorage.getItem(LOCAL_STORAGE_KEY) || "";
+    if (!localhistory) {
+        return;
+    }
     let cities = localhistory.split('\n')
     cities.map(city => {
         $('#citylist').append(
-            `<div id=${city}class="form-inline form-group">
+            `<div id=${city} class="city form-inline form-group">
         <button class="btn btn-primary" id="search-button">${city}
         </button></div>`)
     })
@@ -54,8 +104,10 @@ function loadHistory() {
 
 $(document).ready(function () {
 
+    // load any searches saved from previou
     loadHistory()
 
+    // event handler for search button
     $("#search-button").on("click", function () {
         searchValue = $("#search-value").val();
         console.log(searchValue)
@@ -63,11 +115,10 @@ $(document).ready(function () {
         searchWeather(searchValue, postFiveDayForecast)
     });
 
-    function firstRow(response) {
-        // creating 1st row for current weather
-        let weatherIcon = response.weather.icon;
-        console.log(weatherIcon);
-        let todaysWeather = $("<h2>").text(searchValue + ": " + date + weatherIcon);
+    // event handler for cities (past searches)
+    $(".city").on("click", function (event) {
+        let searchValue = $(this).attr("id");
+        searchWeather(searchValue, postFiveDayForecast)
+    });
 
-    }
 })
